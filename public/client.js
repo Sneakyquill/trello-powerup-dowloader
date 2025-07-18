6fffc916a0eceb243d01061c3c836336828e4785
Sneakyquill <contactsneakyfox@gmail.com>
2025-07-18 15:15:50 +1000

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,13 +1,18 @@
 window.TrelloPowerUp.initialize({
   'board-buttons': function (t, options) {
     return [{
-      icon: 'https://cdn-icons-png.flaticon.com/512/3649/3649467.png',
-      text: 'Download List Images',
+      icon: 'https://webstockreview.net/images/download-icon-png-5.png',
+      text: 'Download List Attachments',
       callback: function (t) {
         return t.popup({
           title: 'Choose List',
-          url: 'index.html',
-          height: 300
+          items: () =>
+            lists.map(list => ({
+              text: list.name,
+              callback: () =>
+downloadImagesFromList(t, list.id)
+            }))
+            )
         });
       }
     }];
