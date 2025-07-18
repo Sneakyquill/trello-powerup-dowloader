b1eb0bd016190c8248e54faccaa2afdbc6f8ea92
Sneakyquill <contactsneakyfox@gmail.com>
2025-07-18 22:47:34 +1000

    Update client.js

--- client.js
+++ client.js
@@ -15,3 +15,26 @@ window.TrelloPowerUp.initialize({
     }];
   }
 });
+window.TrelloPowerUp.initialize({
+  'board-buttons': function (t, options) {
+    return [{
+      icon: 'https://cdn-icons-png.flaticon.com/512/60/60740.png',
+      text: 'Download List Images',
+      callback: function(t) {
+      return
+t.signUrl('https://sneakyquill.github.io/trello-powerup-dowloader/index.html')
+          .then(function(signedUrl) {
+            return t.popup({
+          title: "Select a List",
+          items: () =>
+            t.lists('id', 'name').then(lists =>
+              lists.map(list => ({
+                text: list.name,
+                callback: () => downloadImagesFromList(t, list.id)
+              }))
+            )
+        });
+      }
+    }];
+  }
+});
