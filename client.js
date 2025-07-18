2ca98b6fc81245ed7c1a889d2a09eaa0a853dbca
Sneakyquill <contactsneakyfox@gmail.com>
2025-07-18 22:48:59 +1000

    Update client.js

--- client.js
+++ client.js
@@ -1,21 +1,4 @@
 window.TrelloPowerUp.initialize({
-  'list-actions': function(t, options) {
-    return [{
-      text: 'Download List Images',
-      callback: function(t) {
-        return t.signUrl('https://sneakyquill.github.io/trello-powerup-dowloader/index.html')
-          .then(function(signedUrl) {
-            return t.popup({
-              title: 'Download List Images',
-              url: signedUrl,
-              height: 300
-            });
-          });
-      }
-    }];
-  }
-});
-window.TrelloPowerUp.initialize({
   'board-buttons': function (t, options) {
     return [{
       icon: 'https://cdn-icons-png.flaticon.com/512/60/60740.png',
